a5514fd8d1f1d14ebe68886497920c2c133ca7af
Juma Allan <allanjuma@gmail.com>
2020-07-03 21:26:55 +0300

    setting up app and css

--- app/main.js
+++ app/main.js
@@ -1,11 +1,31 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
 import VueDevtools from 'nativescript-vue-devtools'
+import 'nativescript-tailwind/dist/tailwind.css'
+import store from './store'
+import axios from 'axios'
+import {initializer} from './plugins/axios';
+import {Feedback} from 'nativescript-feedback';
+import Navigator from 'nativescript-vue-navigator';
+import {routes} from './router';
 
-if(TNS_ENV !== 'production') {
+if (TNS_ENV !== 'production') {
   Vue.use(VueDevtools)
 }
-import store from './store'
+global.axios = axios;
+initializer(store);
+
+Vue.prototype.$feedback = new Feedback();
+Vue.registerElement(
+    'PullToRefresh',
+    () => require('@nstudio/nativescript-pulltorefresh').PullToRefresh
+);
+Vue.registerElement(
+    'Fab',
+    () => require('nativescript-floatingactionbutton').Fab
+);
+
+Vue.use(Navigator, {routes});
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
@@ -13,5 +33,5 @@ Vue.config.silent = (TNS_ENV === 'production')
 
 new Vue({
   store,
-  render: h => h('frame', [h(App)])
+  render: h => h(App)
 }).$start()
