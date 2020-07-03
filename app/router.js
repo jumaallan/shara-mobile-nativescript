2591ba2e6b4d85249243dafdae7149f7946e5ed4
Juma Allan <allanjuma@gmail.com>
2020-07-03 21:28:01 +0300

    setting up app router - app navigation

---
+++ app/router.js
@@ -0,0 +1,23 @@
+import Home from '~/components/Pages/Home';
+import Order from '~/components/Pages/Order';
+import Login from '~/components/Pages/Auth/Login';
+import Register from '~/components/Pages/Auth/Register';
+
+export const routes = {
+    '/home': {
+        component: Home,
+        meta: {needsAuth: true}
+    },
+    '/order': {
+        component: Order,
+        meta: {needsAuth: true}
+    },
+    '/register': {
+        component: Register,
+        meta: {needsAuth: false}
+    },
+    '/login': {
+        component: Login,
+        meta: {needsAuth: false}
+    },
+};
